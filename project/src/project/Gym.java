c29a4463c2f56e8951d29c40f618d6cd5965ecdb
Lamiaaz <162748745+Lamiaaz@users.noreply.github.com>
2024-03-13 19:58:33 +0300

    Update Gym.java

--- project/src/project/Gym.java
+++ project/src/project/Gym.java
@@ -4,8 +4,8 @@ public class Gym {
 	
         private String Name;
 	private String Location;
-	private Person [] pList;
-	private Equipment [] eqList;
+	private Person [] peopleList;
+	private Equipment [] equipmentList;
 	private int NumOfPeople;
 	private int NumOfEquipment;
 
@@ -13,8 +13,8 @@ public class Gym {
 	public Gym(String name, String location, int maxPeople , int maxEquipment) {
 		Name = name;
 		Location = location;
-		pList=new Person[maxPeople];
-		eqList=new Equipment[maxEquipment];
+		peopleList=new Person[maxPeople];
+		equipmentList=new Equipment[maxEquipment];
 		NumOfPeople=0;
 		NumOfEquipment=0;
 	}
@@ -22,39 +22,43 @@ public class Gym {
 	
 	public boolean addPerson (Person p) {
 	
-	if(NumOfPeople<pList.length) {
+	if(NumOfPeople<peopleList.length) {
 	
 			if(p instanceof Member) {
-				if(p.getGender()=='F' && p.getAge()>=16)
-				pList[NumOfPeople++]=new Member(p.getName(), p.getAge(), p.getGender(), p.getID(), ((Member)p).getMembershipType(), ((Member)p).getGoal());}
-			else if(p instanceof Trainer) {
-				if(p.getGender()=='F' && p.getAge()>=23)
-				pList[NumOfPeople++] = new Trainer(p.getName(), p.getAge(), p.getGender(), p.getID(), ((Trainer)p).getExperienceYears()); }
-		else if(p instanceof Employee) {
-				if(p.getGender()=='F' && p.getAge()>=21)
-				pList[NumOfPeople++] = new Employee(p.getName(), p.getAge(), p.getGender(), ((Employee)p).getworkHours(), p.getID()); }
+				if((p.getGender()=='F' || p.getGender()=='f') && p.getAge()>=16)
+				peopleList[NumOfPeople++]=new Member(p.getName(), p.getAge(), p.getGender(), p.getID(), ((Member)p).getMembershipType(), ((Member)p).getGoal());
 				return true;
+}
+			else if(p instanceof Trainer) {
+				if((p.getGender()=='F' || p.getGender()=='f') && p.getAge()>=23)
+				peopleList[NumOfPeople++] = new Trainer(p.getName(), p.getAge(), p.getGender(), p.getID(), ((Trainer)p).getExperienceYears());
+				return true;
+}
+		else if(p instanceof Employee) {
+				if((p.getGender()=='F' || p.getGender()=='f') && p.getAge()>=21)
+				peopleList[NumOfPeople++] = new Employee(p.getName(), p.getAge(), p.getGender(), ((Employee)p).getworkHours(), p.getID());
+				return true;
+}
 	}
 	return false; 
 	}
 
 	
-	public int searchPerson(int id){
+	public boolean searchPerson(int id){
 		for(int i=0 ; i<NumOfPeople ; i++){
-		    if(pList[i].getID()==id)		 
-		    return i ;
+		    if(peopleList[i].getID()==id)		 
+		    return true ;
 		}
-		return -1 ; 
+		return false ; 
 	}
+	
 
 	
 	public boolean removePerson(int id) {
-		int loc=searchPerson(id);
-	    if(loc<NumOfPeople && loc>=0){
-	       for(int i=loc;i<NumOfPeople-1;i++) 
-	       pList[i]=pList[i+1];
-	    
-	       pList[--NumOfPeople]=null;
+	       for(int i=0;i<NumOfPeople;i++) 
+	    	   if(peopleList[i].getID()==id) {
+	       peopleList[i]=peopleList[NumOfPeople-1];
+	       peopleList[--NumOfPeople]=null;
 	       return true;
 	    }
            return false;
@@ -64,8 +68,8 @@ public class Gym {
 	
 	
 	public boolean addEquipment(Equipment eq) {
-		if(NumOfEquipment<eqList.length) {
-			eqList[NumOfEquipment++]=eq;
+		if(NumOfEquipment<equipmentList.length) {
+			equipmentList[NumOfEquipment++]=eq;
 		return true;
 		}
 		return false;
@@ -74,63 +78,75 @@ public class Gym {
 	
 	
 	public Equipment[] searchEquipmentByName (String name) {
-		
-		Equipment[] eqName=new Equipment[NumOfEquipment];
+		Equipment[] equipmentName=new Equipment[NumOfEquipment];
 		int j=0;
 		for(int i=0; i<NumOfEquipment; i++) 
-			if(eqList[i].getName().equalsIgnoreCase(name))
-			eqName[j++]=eqList[i];
+			if(equipmentList[i].getName().equalsIgnoreCase(name))
+			equipmentName[j++]=equipmentList[i];
 		
 		if(j==0)
 			return null;
 		else 
-			return eqName;
+			return equipmentName;
 	}
 	
 	
 	
 	public Equipment[] searchEquipmentByType (String type) {
-		Equipment[] eqType=new Equipment[NumOfEquipment];
+		Equipment[] equipmentType=new Equipment[NumOfEquipment];
 		int j=0;
 		for(int i=0; i<NumOfEquipment; i++) 
-			if(eqList[i].getType().equalsIgnoreCase(type))
-			eqType[j++]=eqList[i];
+			if(equipmentList[i].getType().equalsIgnoreCase(type))
+			equipmentType[j++]=equipmentList[i];
 		
 		if(j==0)
 			return null;
 		else 
-			return eqType;
+			return equipmentType;
+	}
+	
+	
+	public boolean removeEquipment(int id) {
+	       for(int i=0;i<NumOfEquipment;i++) 
+	    	   if(equipmentList[i].getID()==id) {
+	    		   equipmentList[i]=equipmentList[NumOfEquipment-1];
+	    		   equipmentList[--NumOfEquipment]=null;
+	       return true;
+	    }
+        return false;
 	}
 
 	
 	public Person[] getEmployees(){
-		Person [] empList=new Person[NumOfPeople];
+		Person [] employeeList=new Person[NumOfPeople];
 		int j=0;
 		for(int i=0;i<NumOfPeople;i++)
-		    if(pList[i] instanceof Employee)
-		    	empList[j++]=new Employee(pList[i].getName(), pList[i].getAge(), pList[i].getGender(), ((Employee)pList[i]).getworkHours(), pList[i].getID());
+		    if(peopleList[i] instanceof Employee)
+		    	employeeList[j++]=new Employee(peopleList[i].getName(), peopleList[i].getAge(), peopleList[i].getGender(), ((Employee)peopleList[i]).getworkHours(), peopleList[i].getID());
 		
-		return empList;
+		return employeeList;
 	}
 	
+	
 	public Person[] getMembers(){
-		Person [] mList=new Person[NumOfPeople];
+		Person [] memberList=new Person[NumOfPeople];
 		int j=0;
 		for(int i=0;i<NumOfPeople;i++)
-		    if(pList[i] instanceof Member)
-		    	mList[j++]=new Member(pList[i].getName(), pList[i].getAge(), pList[i].getGender(), pList[i].getID(), ((Member)pList[i]).getMembershipType(), ((Member)pList[i]).getGoal());
+		    if(peopleList[i] instanceof Member)
+		    	memberList[j++]=new Member(peopleList[i].getName(), peopleList[i].getAge(), peopleList[i].getGender(), peopleList[i].getID(), ((Member)peopleList[i]).getMembershipType(), ((Member)peopleList[i]).getGoal());
 		
-		return mList;
+		return memberList;
 	}
 
+	
 	public Person[] getTrainers(){
-		Person [] tList=new Person[NumOfPeople];
+		Person [] trainerList=new Person[NumOfPeople];
 		int j=0;
 		for(int i=0;i<NumOfPeople;i++)
-		    if(pList[i] instanceof Trainer)
-		    	tList[j++]=new Trainer(pList[i].getName(), pList[i].getAge(), pList[i].getGender(), pList[i].getID(), ((Trainer)pList[i]).getExperienceYears());
+		    if(peopleList[i] instanceof Trainer)
+		    	trainerList[j++]=new Trainer(peopleList[i].getName(), peopleList[i].getAge(), peopleList[i].getGender(), peopleList[i].getID(), ((Trainer)peopleList[i]).getExperienceYears());
 		
-		return tList;
+		return trainerList;
 	}
 
 	
@@ -140,7 +156,7 @@ public class Gym {
 		int num = 0 ;
 		
 		for ( int i = 0 ; i < NumOfPeople ; i++ ) {
-			if ( pList[i] instanceof Member )
+			if ( peopleList[i] instanceof Member )
 				num++ ;
 		}
 		
@@ -151,7 +167,7 @@ public class Gym {
 		int num = 0 ;
 		
 		for ( int i = 0 ; i < NumOfPeople ; i++ ) {
-			if ( pList[i] instanceof Trainer )
+			if ( peopleList[i] instanceof Trainer )
 				num++ ;
 		}
 		
@@ -162,7 +178,7 @@ public class Gym {
 		int num = 0 ;
 		
 		for ( int i = 0 ; i < NumOfPeople ; i++ ) {
-			if ( pList[i] instanceof Employee )
+			if ( peopleList[i] instanceof Employee )
 				num++ ;
 		}
 		
@@ -176,22 +192,22 @@ public class Gym {
 		  //display employees INFO
 		  System.out.println("-----Employees INFO-------");
 		   for(int i=0 ; i< NumOfPeople ; i++)
-		   if ( pList[i] instanceof Employee )
-		    pList[i].Print() ;
+		   if ( peopleList[i] instanceof Employee )
+		    peopleList[i].Print() ;
 		  //display members INFO
 		  System.out.println("-----Members INFO-------");
 		  for(int i=0 ; i< NumOfPeople ; i++)
-		   if ( pList[i] instanceof Member )
-		    pList[i].Print() ;
+		   if ( peopleList[i] instanceof Member )
+		    peopleList[i].Print() ;
 		  //display Trainers INFO
 		  System.out.println("-----Trainers INFO-------");
 		  for(int i=0 ; i< NumOfPeople ; i++)
-		   if ( pList[i] instanceof Trainer )
-		    pList[i].Print() ;
+		   if ( peopleList[i] instanceof Trainer )
+		    peopleList[i].Print() ;
 		  //display Equipments INFO 
 		  System.out.println("-----Equipments INFO-------");
 		  for(int i=0 ; i< NumOfEquipment ; i++)
-		   System.out.print(eqList[i]);
+		   System.out.print(equipmentList[i]);
 
 		   }
 }
