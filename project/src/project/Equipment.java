da68f0bf8323b404fe69d94f54bb34a033fe0a39
Byrem <Byrem@192.168.100.27>
2024-03-13 19:54:19 +0300

    update

--- project/src/project/Equipment.java
+++ project/src/project/Equipment.java
@@ -28,7 +28,7 @@ public class Equipment {
 
 	public String toString() {
 		
-		return "Equipment Name: " + Name + " Type:" + Type + " Quantity: "+ Quantity + " ID: " + ID+ "\n";
+		return "Equipment Name: " + Name + " ID: " + ID+" Type:" + Type + " Quantity: "+ Quantity + "\n";
 	
 	}
 
